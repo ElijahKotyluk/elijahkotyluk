374f905dc0d120930f0e61d61375a91824b3e42a
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-03-30 11:59:28 -0700

    Initialize repo:  - Add and configure @nuxtjs/markdownit dependency  - Add markdownFiles directory to /pages with 2 test .md files  - Add Blog.vue to /pages  - Add /posts/_slug.vue for rendering dynamic blog posts  - Add blog post route links to menu

---
+++ nuxt.config.js
@@ -0,0 +1,114 @@
+import VuetifyLoaderPlugin from 'vuetify-loader/lib/plugin';
+import pkg from './package';
+
+// Vars needed for markdown content:
+const dir = require('node-dir');
+const fs = require('fs');
+const routesArray = [];
+
+var files = fs.readdirSync('./static/markdownFiles');
+
+function createRoutesArray() {
+  files.forEach(function (file) {
+    var name = file.substr(0, file.lastIndexOf('.'));
+    var route = '/posts/' + name
+    routesArray.push(route)
+  });
+}
+
+function returnRoutes() {
+  dir.readFiles('./static/markdownFiles', {
+    match: /.md$/,
+    shortName: true,
+    exclude: /^\./
+    }, function(err, content, next) {
+        if (err) throw err;
+
+        console.log('content:', content);
+
+        next();
+    },
+    function(err, files){
+        if (err) throw err;
+
+        files.forEach(function (file) {
+            var name = file.substr(0, file.lastIndexOf('.'));
+            var path = '/dynamic/' + name
+            return path
+        });
+    });
+}
+
+function getSlugs(post, index) {
+  let slug = post.substr(0, post.lastIndexOf('.'));
+  return `/dynamic/${slug}`
+}
+
+export default {
+  mode: 'universal',
+
+  // Page headers:
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+      }
+    ]
+  },
+
+  // Customize progress-bar color:
+  loading: { color: '#07657e' },
+
+  // Global css:
+  css: [
+    '~/assets/style/app.styl'
+  ],
+
+  // Loaded before app mounts:
+  plugins: [
+    '@/plugins/vuetify'
+  ],
+
+  // Nuxt.js modules:
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    ['@nuxtjs/markdownit', { linkify: true }]
+  ],
+
+  // Axios module:
+  axios: {
+  },
+
+  // Generate static routes:
+  generate: {
+    routes: function() {
+      return files.map(getSlugs)
+    }
+  },
+
+  // Build config:
+  build: {
+    transpile: ['vuetify/lib'],
+    plugins: [new VuetifyLoaderPlugin()],
+    loaders: {
+      stylus: {
+        import: ['~assets/style/variables.styl']
+      }
+    },
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
---
+++ pages/blog.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+      <div v-html="content">
+
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import TestOne from '~/static/markdownFiles/testone.md';
+
+export default {
+  data () {
+    return {
+      content: TestOne
+    }
+  }
+}
+</script>
