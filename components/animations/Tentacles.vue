3c0b8112edb9d0bd4eb08dc7411007146c1f2994
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-04-11 23:46:49 -0700

    Change name of Background.vue component to Tentacles.vue.

---
+++ components/animations/Tentacles.vue
@@ -0,0 +1,28 @@
+<template>
+  <canvas id="tentacles">
+  </canvas>
+</template>
+
+<script>
+import tentacles from '~/plugins/tentacles.js';
+
+export default {
+  name: 'Tentacles',
+  mounted() {
+    tentacles();
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#tentacles {
+  top: 0px;
+  left: 0px;
+	width: 100%;
+	height: 100%;
+	cursor: pointer;
+	position: absolute;
+  background-size: cover;
+	background: "rgba(0, 0, 0, 0)";
+}
+</style>
