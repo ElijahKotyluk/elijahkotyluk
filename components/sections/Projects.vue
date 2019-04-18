401b1fbdb1d5826281b581b17bc8cafd89a558ea
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-04-17 23:53:09 -0700

    Add projects data to Projects.vue

--- components/sections/Projects.vue
+++ components/sections/Projects.vue
@@ -1,6 +1,20 @@
 <template>
   <v-layout>
     <v-flex>
+      <v-card v-for="(project, i) in projects" xs12 sm6 md6>
+        <p>
+          {{ project.title }}
+        </p>
+        <p>
+          {{ project.description }}
+        </p>
+        <p>
+          {{ project.stack }}
+        </p>
+        <a>
+          {{ project.link }}
+        </a>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
@@ -9,7 +23,28 @@
 export default {
   name: 'Projects',
   data () {
-    return {}
+    return {
+      projects: [
+        {
+          title: 'bekn',
+          description: 'social platform prototype.',
+          stack: 'Vue.js, Firebase, Flask, NDB',
+          link: 'link'
+        },
+        {
+          title: 'vue-moment-calendar',
+          description: 'event calendar made with vue and moment.',
+          stack: 'Vue.js, Moment.js, Flask, NDB',
+          link: 'https://vue-moment-calendar.appspot.com/'
+        },
+        {
+          title: 'vue-weather-app',
+          description: 'Simple weather application built with Vue.js.',
+          stack: 'Vue.js & Vuetify',
+          link: 'https://vue-openweather.appspot.com/'
+        }
+      ]
+    }
   }
 }
 </script>
